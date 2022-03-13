94dbb78fe51debe4d1064092e4c07c580c926e23
Alexis Aravena <aravenapro98@gmail.com>
2022-03-13 17:33:26 -0300

    Jwt utils and mapper

---
+++ FeriaVirtual.API/Authorization/JwtUtils.cs
@@ -0,0 +1,69 @@
+﻿using FeriaVirtual.API.Helpers;
+using FeriaVirtual.Database.Entities;
+using Microsoft.Extensions.Options;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+
+namespace FeriaVirtual.API.Authorization
+{
+    public interface IJwtUtils
+    {
+        public string GenerateToken(User user);
+        public int? ValidateToken(string token);
+    }
+
+    public class JwtUtils : IJwtUtils
+    {
+        private readonly JwtSettings _jwtSettings;
+
+        public JwtUtils(IOptions<JwtSettings> jwtSettings)
+        {
+            _jwtSettings = jwtSettings.Value;
+        }
+
+        public string GenerateToken(User user)
+        {
+            var tokenHandler = new JwtSecurityTokenHandler();
+            var key = Encoding.ASCII.GetBytes(_jwtSettings.Secret);
+            var tokenDescriptor = new SecurityTokenDescriptor
+            {
+                Subject = new System.Security.Claims.ClaimsIdentity(new[] { new Claim("id", user.Id.ToString()) }),
+                Expires = DateTime.Now.AddDays(1),
+                SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(key), SecurityAlgorithms.HmacSha256Signature)
+            };
+            var token = tokenHandler.CreateToken(tokenDescriptor);
+            return tokenHandler.WriteToken(token);
+        }
+
+        public int? ValidateToken(string token)
+        {
+            if (token == null) return null;
+            var tokenHandler = new JwtSecurityTokenHandler();
+            var key = Encoding.ASCII.GetBytes(_jwtSettings.Secret);
+            try
+            {
+                tokenHandler.ValidateToken(token, new TokenValidationParameters
+                {
+                    ValidateIssuerSigningKey = true,
+                    IssuerSigningKey = new SymmetricSecurityKey(key),
+                    ValidateIssuer = false,
+                    ValidateAudience = false,
+                    ClockSkew = TimeSpan.Zero,
+                }, out SecurityToken validatedToken);
+
+                var jwtToken = (JwtSecurityToken)validatedToken;
+                var userId = int.Parse(jwtToken.Claims.First(usr => usr.Type == "id").Value);
+
+                // return user id from JWT token if validation successful
+                return userId;
+            }catch(Exception ex)
+            {
+                // return null if validation fails
+                return null;
+            }
+
+        }
+    }
+}
---
+++ FeriaVirtual.API/Helpers/AutoMapperProfile.cs
@@ -0,0 +1,30 @@
+﻿namespace FeriaVirtual.API.Helpers
+{
+    using AutoMapper;
+    using FeriaVirtual.Database.Entities;
+    using FeriaVirtual.API.Models.Users;
+    public class AutoMapperProfile : Profile
+    {
+        public AutoMapperProfile()
+        {
+            // User -> AuthenticateResponse
+            CreateMap<User, AuthenticateResponse>();
+
+            // RegisterRequest -> User
+            CreateMap<RegisterRequest, User>();
+
+            //UpdateRequest -> User
+            CreateMap<UpdateRequest, User>()
+            .ForAllMembers(x => x.Condition(
+                (src, dest, prop) =>
+                {
+                    // ignore null & empty string properties
+                    if (prop == null) return false;
+                    if (prop.GetType() == typeof(string) && string.IsNullOrEmpty((string)prop)) return false;
+
+                    return true;
+                }
+            ));
+        }
+    }
+}
