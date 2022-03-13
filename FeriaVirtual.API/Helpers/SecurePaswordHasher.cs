e3f42cd7c0e82f650e6d0f625c6e1b47cc814412
Alexis Aravena <aravenapro98@gmail.com>
2022-03-13 17:32:33 -0300

    Hashing

---
+++ FeriaVirtual.API/Helpers/SecurePaswordHasher.cs
@@ -0,0 +1,17 @@
+﻿using System.Security.Cryptography;
+using System.Text;
+
+namespace FeriaVirtual.API.Helpers
+{
+    public static class SecurePaswordHasher
+    {
+        public static string ComputeHash(string input)
+        {
+            var bytes = Encoding.UTF8.GetBytes(input);
+            byte[] hasBytes;
+
+            hasBytes = SHA512.Create().ComputeHash(bytes);
+            return Convert.ToBase64String(hasBytes);
+        }
+    }
+}
